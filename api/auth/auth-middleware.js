043eb6653f10dcbf7d8e1b54567be5dee62d1e19
Jordan <jordan.k.wilia@gmail.com>
2022-09-23 14:55:10 -0700

    completed

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,71 @@
+const User = require('../users/users-model')
+
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next()
+  } else {
+    next({
+      status: 401,
+      message: 'You shall not pass!'
+    })
+  }
+}
+
+function checkBody(req, res, next) {
+    if (!req.body.username && !req.body.password) {
+        next({
+            status: 401,
+            message: 'username and password required'
+        })
+    }
+}
+
+async function checkUsernameFree(req, res, next) {
+  try {
+    const users = await User.findBy({username: req.body.username})
+    if (!users.length) {
+      next()
+    } else {
+      next({
+        status: 422,
+        message: "Username taken"
+      })}
+  } catch (err) {
+    next(err)
+  }
+}
+
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({username: req.body.username})
+    if (users.length) {
+      req.user = users[0]
+      next()
+    } else {
+      next({
+        status: 401,
+        message: "Invalid credentials"
+      })}
+  } catch (err) {
+    next(err)
+  }
+}
+
+function checkPasswordLength(req, res, next) {
+  if (!req.body.password || req.body.password.length < 3) {
+    next({
+      status: 422,
+      message: "Password must be longer than 3 chars"
+    })
+  } else {
+    next()
+  }
+}
+
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+  checkBody,
+}
--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session')
+const Store = require('connect-session-knex')(session)
+const knex = require('../data/dbConfig')
 
 const restrict = require('./middleware/restricted.js');
 
@@ -9,11 +12,42 @@ const jokesRouter = require('./jokes/jokes-router.js');
 
 const server = express();
 
+server.use(session({
+    name: 'chocolatechip',
+    secret: 'shh',
+    saveUninitialized: false,
+    resave: false,
+    store: new Store({
+      knex,
+      createTable: true,
+      clearInterval: 1000 * 60 * 10,
+      tablename: 'sessions',
+      sidfieldname: 'sid',
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 10,
+      secure: false,
+      httpOnly: true,
+      // sameSite: 'none'
+    }
+  }))
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
+
+server.get("/", (req, res) => {
+    res.json({ api: "up" });
+  });
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    });
+  });
 
 module.exports = server;
